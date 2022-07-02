4260eab8ded5446f0153f5034a3662501671639e
Praveen11July <106472504+Praveen11July@users.noreply.github.com>
2022-07-02 18:59:59 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      <input type="text" id="input"></input>
+
+      //End code
+      });});}
+getData();
